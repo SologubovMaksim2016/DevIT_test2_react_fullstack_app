0760b4fbec03c946127e7d5c5c6082b7ed845ded
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2021-12-10 15:51:25 +0200

    Update auth.routes.js

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -10,7 +10,7 @@ const {
 } = require("express-validator")
 const router = new Router()
 const authMiddleware = require('../middleware/auth.middleware')
-const corsMiddleware = require('./middleware/cors.middleware')
+const corsMiddleware = require('../middleware/cors.middleware')
 /**
  *    @swagger 
  * /auth/registration:
