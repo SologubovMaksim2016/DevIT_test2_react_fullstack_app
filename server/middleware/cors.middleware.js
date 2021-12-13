a03bcf47280c62b449725679b45ea0abab62b2a9
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2021-12-13 13:50:02 +0200

    Update cors.middleware.js

--- server/middleware/cors.middleware.js
+++ server/middleware/cors.middleware.js
@@ -2,6 +2,7 @@ function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, content-type, authorization");
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 }
 
